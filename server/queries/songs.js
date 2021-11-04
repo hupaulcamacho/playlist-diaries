335af7843238067be82ae6ec32fc81373a7f8053
Hupaul <hupaulcamacho@pursuit.org>
2021-11-03 20:51:45 -0400

    add create song functionality in database

--- server/queries/songs.js
+++ server/queries/songs.js
@@ -1,24 +1,25 @@
 const db = require('../db/server.js');
 
 const getAllSongs = async () => {
-    try {
-      const allSongs = await db.any("SELECT * FROM songs");
-      return allSongs; 
-    } catch (error) {
-        return error
-    }
+  const allSongs = await db.any("SELECT * FROM songs");
+  return allSongs; 
 }
 
 const getSongById = async (id) => {
-  try {
-    const song = await db.oneOrNone("SELECT * FROM songs WHERE id = $1", [ id ]);
-    return song
-  } catch (error) {
-    return error  
-  }
+  const song = await db.oneOrNone("SELECT * FROM songs WHERE id = $1", [ id ]);
+  return song
+}
+
+const createNewSong = async (song) => {
+  const insertQuery = `INSERT INTO songs (name, artist, album, time, is_favorite) VALUES ($1, $2, $3, $4, $5) RETURNING *`
+  
+  const newSong = await db.one(insertQuery, [song.name, song.artist, song.album, song.time, song.is_favorite]);
+  
+  return newSong
 }
 
 module.exports = { 
   getAllSongs, 
-  getSongById 
+  getSongById,
+  createNewSong 
 };
--- server/routes/songs.js
+++ server/routes/songs.js
@@ -1,19 +1,67 @@
 const express = require('express');
 const router = express.Router();
-const { getAllSongs, getSongById } = require("../queries/songs");
+const { getAllSongs, getSongById, createNewSong } = require("../queries/songs");
 
 // get all songs in database
 router.get('/', async (req, res) => {
-    const allSongs = await getAllSongs();
-    res.json(allSongs); 
+    try {
+        const allSongs = await getAllSongs();
+        res.json({
+            payload: allSongs,
+            message: 'songs retrieved',
+            err: false
+        })    
+    } catch (error) {
+        res.status(500).json({
+            payload: null,
+            message: 'songs not retrieved',
+            err: true
+        }) 
+    }
 })
 
 // get single song by id
 router.get('/:id', async (req, res) => {
-    const song = await getSongById(req.params.id);
-    res.json(song); 
+    try {
+        const song = await getSongById(req.params.id);
+        res.json({
+            payload: song,
+            message: 'song retrieved',
+            err: false
+        })    
+    } catch (error) {
+        res.status(500).json({
+            payload: null,
+            message: 'song not retrieved',
+            err: true
+        }) 
+    }
 })
 
+// create new song
+router.post('/', async (req, res) => {
+    const songInfo = {
+        name: req.body.name,
+        artist: req.body.artist,
+        album: req.body.album,
+        time: req.body.time,
+        is_favorite: req.body.is_favorite
+    }
 
+    try {
+        let newSong = await createNewSong(songInfo);
+        res.json({
+            payload: newSong,
+            message: 'new song created',
+            error: false
+        })
+    } catch (error) {
+        res.json({
+            payload: null,
+            message: 'An error has occured.',
+            error: true
+        })
+    }
+})
 
 module.exports = router
