abf016f27b8a14557aaaea078eb2a9574b957d5b
Hupaul <hupaulcamacho@pursuit.org>
2021-11-03 23:33:32 -0400

    implement network request to backend to get song info

--- client/src/components/songs.js
+++ client/src/components/songs.js
@@ -1,10 +1,36 @@
-import React, { Component } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 function Songs(){
+    const [ songs, setSongs ] = useState([]);
+
+
+    // functions
+    const getSongs = async () => {
+        try {
+            const { data } = await axios.get('http://localhost:3001/songs/')
+            let songs = data.payload
+            console.log(songs)
+            let songComponents = []
+            for (let i = 0; i < songs.length; i++) {
+                songComponents.push(
+                    <div className='song'>
+                        <h4>{songs[i].name}</h4>
+                    </div>
+                )
+            }
+            setSongs(songComponents)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        getSongs()
+    }, [])
 
     return (
         <div className='main'>
             <h1>Songs</h1> 
+            {songs}
         </div>
     )
 
